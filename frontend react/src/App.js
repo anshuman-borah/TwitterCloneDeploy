ada7c77431781a86335bf135797da5b983cee8c9
anshuman-borah <anshuman15b@gmail.com>
2026-08-27 11:20:14 +0530

    Update App.js

--- frontend react/src/App.js
+++ frontend react/src/App.js
@@ -5,49 +5,48 @@ import HomePage from './Components/HomePage';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { getUserProfile } from './Store/Auth/Action';
-
 import darkTheme from './Theme/DarkTheme';
 import lightTheme from './Theme/LightTheme';
-import { Box, Button, CssBaseline } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Palette } from '@mui/icons-material';
+import { Box, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import VerifiedSuccess from './Components/VerifiedSuccess/VerifiedSuccess';
 
 function App() {
-  const dispatch=useDispatch();
-  const {auth}=useSelector(store=>store);
-  const jwt = localStorage.getItem("jwt")
-  const [currentTheme,setCurrentTheme]=useState("");
-  const {theme}=useSelector(store=>store);
+  const dispatch = useDispatch();
+  const { auth } = useSelector((store) => store);
+  const jwt = localStorage.getItem("jwt");
+  const [currentTheme, setCurrentTheme] = useState("");
+  const { theme } = useSelector((store) => store);
 
-  useEffect(()=>{
-
-    if(jwt){
-      dispatch(getUserProfile(jwt))
+  useEffect(() => {
+    if (jwt) {
+      dispatch(getUserProfile(jwt));
     }
-  
-  },[auth.jwt,jwt])
+  }, [jwt, dispatch]);
 
-  useEffect(()=>{
-setCurrentTheme(localStorage.getItem("theme"))
-  },[theme.currentTheme])
- 
-  console.log("them ",theme.currentTheme)
+  useEffect(() => {
+    setCurrentTheme(localStorage.getItem("theme"));
+  }, [theme.currentTheme]);
+
+  console.log("theme ", theme.currentTheme);
+
   return (
-    <ThemeProvider theme={currentTheme==="dark"? darkTheme:lightTheme} className="">
+    <ThemeProvider theme={currentTheme === "dark" ? darkTheme : lightTheme}>
       <CssBaseline />
-      <Box sx={{}}>
-        {/* <Button variant='content' color='success'>Check Theme</Button> */}
-          <Routes>
-        <Route path='/*' element={ auth.user?.fullName? <HomePage/>:<Authentication/>}></Route>
-        <Route path='/signin' element={<Authentication/>}></Route>
-        <Route path='/signup' element={<Authentication/>}></Route>
-        <Route path='/verified' element={<VerifiedSuccess/>}></Route>
-        {/* <Route path='/profile' element={<HomePage/>}></Route> */}
-      </Routes>
+      <Box>
+        <Routes>
+          {/* Auth routes */}
+          <Route path="/signin" element={<Authentication />} />
+          <Route path="/signup" element={<Authentication />} />
+          <Route path="/verified" element={<VerifiedSuccess />} />
+
+          {/* Everything else handled by HomePage (which contains its own routes) */}
+          <Route
+            path="/*"
+            element={auth.user?.fullName ? <HomePage /> : <Authentication />}
+          />
+        </Routes>
       </Box>
-    
-      
     </ThemeProvider>
   );
 }
