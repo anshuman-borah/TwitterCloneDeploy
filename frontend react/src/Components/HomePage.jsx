2f6d22cc4b916ef278a0548377c1456236e811f2
anshuman-borah <anshuman15b@gmail.com>
2026-08-27 11:23:11 +0530

    Update HomePage.jsx

--- frontend react/src/Components/HomePage.jsx
+++ frontend react/src/Components/HomePage.jsx
@@ -3,7 +3,7 @@ import { Grid } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./Navigation/Navigation";
 import HomeSection from "./Home/MiddlePart/HomeSection";
-import RightPart from "./Home/RightPart/RightPart";
+import RightPart from "./RightPart/RightPart";
 import Profile from "./Profile/Profile";
 import TwitDetail from "./Home/MiddlePart/TwitDetail/TwitDetail";
 import Explore from "./Home/MiddlePart/Explore";
@@ -14,11 +14,16 @@ import Communities from "./Home/MiddlePart/Communities";
 
 const HomePage = () => {
   return (
-    <Grid container xs={12} className="px-5 lg:px-36 justify-between">
+    <Grid container className="px-5 lg:px-36 justify-between">
       <Grid item xs={0} lg={2.5} className="hidden lg:block w-full relative">
         <Navigation />
       </Grid>
-      <Grid item xs={12} lg={6} className="px-5 lg:px-9 border-l border-r border-gray-700 min-h-screen">
+      <Grid
+        item
+        xs={12}
+        lg={6}
+        className="px-5 lg:px-9 border-l border-r border-gray-700 min-h-screen"
+      >
         <Routes>
           <Route path="/" element={<HomeSection />} />
           <Route path="/home" element={<HomeSection />} />
